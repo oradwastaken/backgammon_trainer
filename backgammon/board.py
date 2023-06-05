b83219b03cb8a596e7c185876ef9c6d5c2220d0c
Orad Reshef <orad@reshef.ca>
2023-06-05 10:04:02 -0400

    Updated documentation

--- backgammon/board.py
+++ backgammon/board.py
@@ -1,12 +1,9 @@
 from dataclasses import dataclass
-
 from enum import StrEnum
-from typing import Optional
-
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 
-class Pipcount(NamedTuple):
+class PipCount(NamedTuple):
     X: int
     O: int
 
@@ -18,16 +15,21 @@ class Team(StrEnum):
 
 @dataclass(slots=True)
 class Point:
+    """Represents a point on the board."""
     number: int
     num_checkers: int = 0
     color: Optional[Team] = None
 
 
 class Board:
+    """A backgammon board, comprised of 24 points and the bar point (0).
+    The X team represents the user, and the O team represents the opponent."""
+
     def __init__(self):
         self.points = [Point(i) for i in range(25)]
 
-    def setup(self):
+    def setup(self) -> None:
+        """Sets up the standard initial backgammon board."""
         self.points[24].num_checkers = 2
         self.points[24].color = Team.X
 
@@ -52,14 +54,16 @@ class Board:
         self.points[19].num_checkers = 5
         self.points[19].color = Team.O
 
-    def display(self):
+    def display(self) -> None:
+        """Displays the board in the command line."""
         for point in self.points:
             if point.num_checkers:
                 print(f"{point.number: 03d}: {point.color * point.num_checkers}")
             else:
                 print(f"{point.number: 03d}:")
 
-    def get_pipcount(self) -> Pipcount:
+    def get_pipcount(self) -> PipCount:
+        """Calculates and returns the pip count for each player."""
         X_count = sum(point.number * point.num_checkers for point in self.points if point.color == Team.X)
         O_count = sum((25 - point.number) * point.num_checkers for point in self.points if point.color == Team.O)
-        return Pipcount(X=X_count, O=O_count)
+        return PipCount(X=X_count, O=O_count)
