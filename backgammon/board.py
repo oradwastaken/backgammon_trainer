7395bf0b9c789900ded89f1303b67d6f84fc4500
Orad Reshef <orad@reshef.ca>
2023-06-25 21:49:24 -0400

    Started implementing simple checker moves

--- backgammon/board.py
+++ backgammon/board.py
@@ -1,10 +1,11 @@
 import random
-from collections import defaultdict
 from dataclasses import dataclass
 from enum import StrEnum
 from typing import NamedTuple, Optional
 
-from backgammon import colors
+
+class InvalidMove(Exception):
+    """Raised when user tries to make an invalid checker play"""
 
 
 class PipCount(NamedTuple):
@@ -71,38 +72,28 @@ class Board:
         self.points[point_num].num_checkers = 1
         self.points[point_num].color = Team.X
 
-    def random_board(self):
+    def random_board(self, bar: bool = False, max_checkers: int = 5):
+        self.reset()
         touched_points = []
 
-        remaining_checkers = 15
-        while remaining_checkers > 0:
-            point_num = random.randint(0, 24)
-            if point_num in touched_points:
-                continue
+        for color in [Team.X, Team.O]:
+            remaining_checkers = 15
+            while remaining_checkers > 0:
+                if bar:
+                    point_num = random.randint(0, 26)
+                else:
+                    point_num = random.randint(1, 25)
+                if point_num in touched_points:
+                    continue
 
-            touched_points.append(point_num)
-            num_checkers = random.randint(1, 6)
-            if num_checkers > remaining_checkers:
-                num_checkers = remaining_checkers
-            remaining_checkers = remaining_checkers - num_checkers
+                touched_points.append(point_num)
+                num_checkers = random.randint(1, max_checkers)
+                if num_checkers > remaining_checkers:
+                    num_checkers = remaining_checkers
+                remaining_checkers = remaining_checkers - num_checkers
 
-            self.points[point_num].num_checkers = num_checkers
-            self.points[point_num].color = Team.X
-
-        remaining_checkers = 15
-        while remaining_checkers > 0:
-            point_num = random.randint(0, 24)
-            if point_num in touched_points:
-                continue
-
-            touched_points.append(point_num)
-            num_checkers = random.randint(1, 6)
-            if num_checkers > remaining_checkers:
-                num_checkers = remaining_checkers
-            remaining_checkers = remaining_checkers - num_checkers
-
-            self.points[point_num].num_checkers = num_checkers
-            self.points[point_num].color = Team.O
+                self.points[point_num].num_checkers = num_checkers
+                self.points[point_num].color = color
 
     @property
     def points_with_checkers(self) -> list[Point]:
@@ -115,57 +106,40 @@ class Board:
         O_count = sum((25 - point.number) * point.num_checkers for point in self.points if point.color == Team.O)
         return PipCount(X=X_count, O=O_count)
 
+    def move_checkers(self, from_pt: int, to_pt: int, num_checkers: int = 1):
+        from_pt = self.points[from_pt]
+        to_pt = self.points[to_pt]
+        if num_checkers > from_pt.num_checkers:
+            raise InvalidMove(f"Not enough checkers on point ({from_pt.num_checkers}) to move {num_checkers} checkers")
 
-def print_board(board: Board, show_points=True):
-    labels = defaultdict(lambda: '  ')
-    labels.update(
-            {
-                    'BAR': colors.y("BAR"),
-                    'OFF': colors.y("OFF"),
-                    'even': colors.y("░░"),
-                    'odd': '░░',
-                    }
-            )
+        if from_pt.color != to_pt.color and to_pt.color is not None:
+            if to_pt.num_checkers > 1:
+                raise InvalidMove(
+                    f"Opponent is in control of the {to_pt.number}-point with {to_pt.num_checkers} checkers"
+                    )
+            else:
+                self.hit_checker(to_pt.number)
 
-    if show_points:
-        labels.update({f'p{i}': f"{i:02}" for i in range(1, 25)})
+        for _ in range(num_checkers):
+            to_pt.num_checkers += 1
+            from_pt.num_checkers -= 1
+        to_pt.color = from_pt.color
 
-    X = colors.r(" X")
-    O = colors.g(" O")
-    XX = colors.r("XX")
-    OO = colors.g("OO")
+        if from_pt.num_checkers == 0:
+            from_pt.color = None
 
-    for point in board.points_with_checkers:
-        checker = X if point.color == Team.X else O
 
-        for i in range(point.num_checkers):
-            labels.update({f"{point.number}_{i + 1}": checker})
+    def hit_checker(self, pt: int) -> None:
+        point = self.points[pt]
+        if point.num_checkers > 1:
+            raise InvalidMove(f"Too many checkers to hit ({point.num_checkers})")
+        if point.num_checkers < 1:
+            raise InvalidMove(f"No checkers to hit on {point.number} point.")
 
         if point.color == Team.X:
-            checker = XX
-        else:
-            checker = OO
-
-        for i in range(5, point.num_checkers):
-            labels.update({f"{point.number}_{i % 5 + 1}": checker})
-
-    board = '\n'
-    board += '║ {p13} ║ {p14} ║ {p15} ║ {p16} ║ {p17} ║ {p18} ║ {BAR} ║ {p19} ║ {p20} ║ {p21} ║ {p22} ║ {p23} ║ {p24} ║ {OFF} ║\n'
-    board += '╠════╩════╩════╩════╩════╩════╬═════╬════╩════╩════╩════╩════╩════╬═════╣\n'
-    board += '║ {odd}   {even}   {odd}   {even}   {odd}   {even} ║     ║ {odd}   {even}   {odd}   {even}   {odd}   {even} ║     ║\n'
-    board += '║ {13_1}   {14_1}   {15_1}   {16_1}   {17_1}   {18_1} ║     ║ {19_1}   {20_1}   {21_1}   {22_1}   {23_1}   {24_1} ║     ║\n'
-    board += '║ {13_2}   {14_2}   {15_2}   {16_2}   {17_2}   {18_2} ║     ║ {19_2}   {20_2}   {21_2}   {22_2}   {23_2}   {24_2} ║     ║\n'
-    board += '║ {13_3}   {14_3}   {15_3}   {16_3}   {17_3}   {18_3} ║     ║ {19_3}   {20_3}   {21_3}   {22_3}   {23_3}   {24_3} ║     ║\n'
-    board += '║ {13_4}   {14_4}   {15_4}   {16_4}   {17_4}   {18_4} ║  {0_1} ║ {19_4}   {20_4}   {21_4}   {22_4}   {23_4}   {24_4} ║     ║\n'
-    board += '║ {13_5}   {14_5}   {15_5}   {16_5}   {17_5}   {18_5} ║     ║ {19_5}   {20_5}   {21_5}   {22_5}   {23_5}   {24_5} ║     ║\n'
-    board += '║                             ║ {BAR} ║                             ║     ║\n'
-    board += '║ {12_5}   {11_5}   {10_5}   {9_5}   {8_5}   {7_5} ║     ║ {6_5}   {5_5}   {4_5}   {3_5}   {2_5}   {1_5} ║     ║\n'
-    board += '║ {12_4}   {11_4}   {10_4}   {9_4}   {8_4}   {7_4} ║  {25_1} ║ {6_4}   {5_4}   {4_4}   {3_4}   {2_4}   {1_4} ║     ║\n'
-    board += '║ {12_3}   {11_3}   {10_3}   {9_3}   {8_3}   {7_3} ║     ║ {6_3}   {5_3}   {4_3}   {3_3}   {2_3}   {1_3} ║     ║\n'
-    board += '║ {12_2}   {11_2}   {10_2}   {9_2}   {8_2}   {7_2} ║     ║ {6_2}   {5_2}   {4_2}   {3_2}   {2_2}   {1_2} ║     ║\n'
-    board += '║ {12_1}   {11_1}   {10_1}   {9_1}   {8_1}   {7_1} ║     ║ {6_1}   {5_1}   {4_1}   {3_1}   {2_1}   {1_1} ║     ║\n'
-    board += '║ {even}   {odd}   {even}   {odd}   {even}   {odd} ║     ║ {even}   {odd}   {even}   {odd}   {even}   {odd} ║     ║\n'
-    board += '╠════╦════╦════╦════╦════╦════╬═════╬════╦════╦════╦════╦════╦════╬═════╣\n'
-    board += '║ {p12} ║ {p11} ║ {p10} ║ {p9} ║ {p8} ║ {p7} ║ {BAR} ║ {p6} ║ {p5} ║ {p4} ║ {p3} ║ {p2} ║ {p1} ║ {OFF} ║\n'
-
-    print(board.format_map(labels))
+            self.points[25].num_checkers += 1
+            self.points[25].color = Team.X
+        elif point.color == Team.O:
+            self.points[0].num_checkers += 1
+            self.points[0].color = Team.O
+        point.num_checkers = 0
--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+from backgammon.board import Board
 from backgammon.games import point_number_game
+from backgammon.shell import print_board
 
 
 def main():
+    # Play the point_number quiz game!
     point_number_game()
+
+
+    # Here's examples of how to manipulate the board:
+    board = Board(bear_off_left=True)
+
+    print('Original board')
+    board.setup()
+    print_board(board)
+
+    print('Moving 24/13')
+    board.move_checkers(24, 13)
+    print_board(board)
+
+    print('Moving 12/14, 1217')
+    board.move_checkers(12, 14)
+    board.move_checkers(12, 17)
+    print_board(board)
+
+    print('Moving 24/14')
+    board.move_checkers(24, 14)
+    print_board(board)
+
+    print('Moving bar to 5')
+    board.move_checkers(0, 5)
+    print_board(board)
 
 
 if __name__ == '__main__':
