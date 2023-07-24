656583fb00fb11c2d9c2c23fe93c146e51c619cb
Orad Reshef <orad@reshef.ca>
2023-07-24 09:30:35 -0400

    Parametrized tests. Cool feature

--- tests/shell_test.py
+++ tests/shell_test.py
@@ -1,21 +1,29 @@
+import pytest
+
 from bgtrainer import shell
 from bgtrainer.board import Move
 
 
-def test_parse_multiple_moves():
-    assert len(shell.parse_moves("24/11")) == 1
-    assert len(shell.parse_moves("24/11, 11/13")) == 2
-    assert len(shell.parse_moves("24/11, 13/11, 11/5")) == 3
+@pytest.mark.parametrize(
+    "test_input, expected_output", [("24/11", 1), ("24/11, 11/13", 2), ("24/11, 13/11, 11/5", 3)]
+)
+def test_parse_multiple_moves(test_input, expected_output):
+    assert len(shell.parse_moves(test_input)) == expected_output
 
 
-def test_parse_exact_moves():
-    assert shell.parse_moves("24/11")[0] == Move(24, 11)
-    assert shell.parse_moves("10/5") == [Move(10, 5)]
-    assert shell.parse_moves("24/11, 5/1") == [Move(24, 11), Move(5, 1)]
+@pytest.mark.parametrize(
+    "test_input, expected_output",
+    [
+        ("24/11, 5/1", [Move(24, 11), Move(5, 1)]),  # multiple moves
+        ("13/8", [Move(13, 8)]),  # without brackets
+        ("(13/8)", [Move(13, 8)]),  # with brackets
+        ("(13/8", [Move(13, 8)]),  # only one bracket
+        ("   (13/8)    ", [Move(13, 8)]),  # lots of white space
+    ],
+)
+def test_parse_moves(test_input, expected_output):
+    assert shell.parse_moves(test_input) == expected_output
 
 
-def test_parse_moves_variations():
-    assert shell.parse_moves("(13/8)") == [Move(13, 8)]
-    assert shell.parse_moves("   (13/8)   ") == [Move(13, 8)]
-    assert shell.parse_moves("(13/8") == [Move(13, 8)]
-    assert shell.parse_moves("13/8") == [Move(13, 8)]
+# def test_parse_moves_errors():
+#     assert shell.parse_moves("(138)") == []
